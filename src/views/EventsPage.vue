1859d3ee7500300617357ca0cb05657a2682dd75
Harshit Katiyar <harshasterhk@gmail.com>
2023-09-17 18:54:45 +0530

    I hope kuch add kiya ho

--- src/views/EventsPage.vue
+++ src/views/EventsPage.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container" id="events">
-        <h1 class="text-white">
-            Hello there this is events section
-        </h1>
+        <div class="row justify-content-start">
+            <EventVue v-for="event in events" :event="event" :key="event.id"></EventVue>
+        </div>
     </div>
 </template>
 
@@ -11,3 +11,36 @@
     min-height: 100vh;
 }
 </style>
+
+<script>
+    import EventVue from '@/components/EventVue.vue'
+    export default{
+        data(){
+            return {
+                events : [
+                    {
+                        "id" : 1,
+                        "title" : "Event1",
+                        "col" : 5,
+                        "description" : "This is event 1"
+                    },
+                    {
+                        "id" : 2,
+                        "title" : "Event2",
+                        "col" : 7,
+                        "description" : "This is event 2"
+                    },
+                    {
+                        "id" : 3,
+                        "title" : "Event3",
+                        "col" : 4,
+                        "description" : "This is event 3"
+                    }
+                ]
+            }
+        },
+        components : {
+            EventVue
+        }
+    }
+</script>
