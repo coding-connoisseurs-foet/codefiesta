afd2f338ec279cc07384f977631bcad73d292c81
Harshvardhan Singh <harshvardhansingh1613@gmail.com>
2024-02-24 18:39:10 +0530

    fixed the issue with URL by updating the router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 const routes = [
   {
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
