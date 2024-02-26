867ce5dc2dfec4cc43c7fe5b3492e6716d6bdacc
Kritika Singh <kritikasingh00.com@gmail.com>
2024-02-26 18:53:09 +0530

    postman page

--- src/components/seminar/PostmanSeminar.vue
+++ src/components/seminar/PostmanSeminar.vue
@@ -4,24 +4,40 @@
       <span>{{ event.name }}</span>
     </h1>
     <p><span>Description : </span> {{ event.overview }}</p>
-    <p class="rule-highlight">Description of the round :</p>
-    <div class="speaker">
-      <div class="col-6 col-md-4 col-lg-3 mt-lg-3 mt-md-3 pb-5 anime"  v-for="speaker in event.speakers" :key="speaker.title">
-        <div class="card text-center text-white bg-dark ">
-          <img :src="`https://github.com/${speaker.github}.png`" class="card-img-top" :alt="imageAlt" />
-          <div class="card-body">
-            <h5 class="card-title">{{ speaker.title }}</h5>
+
+    <!-- ---------speaker card --------- -->
+    <p class="highlight">Meet the Speakers :</p>
+    <div class="speaker d-flex align-items-center justify-content-evenly w-100">
+      <div
+        class="pb-5 anime"
+        v-for="speaker in event.speakers"
+        :key="speaker.title"
+      >
+        <div class="card text-center text-white bg-dark w-302">
+          <img
+            :src="`https://github.com/${speaker.github}.png`"
+            class="card-img-top"
+            :alt="imageAlt"
+          />
+          <div class="card-body w-100  d-flex flex-column align-items-center">
+            <h5 class="card-title w-100">{{ speaker.title }}</h5>
             <p class="card-text">
               <a
                 class="btn border"
                 :href="`https://github.com/${speaker.github}`"
                 target="_blank"
-                >View On Github</a>
+                >View On Github</a
+              >
             </p>
           </div>
         </div>
       </div>
     </div>
+    <!-- ---------speaker card --------- -->
+    <p class="highlight">About Postman :</p>
+    <p>{{ event.about }}</p>
+    
+    <p><span>Platform : </span> <a :href="event.link">www.postman.com</a></p>
     <div id="main-announce" class="w-100">
       <a
         href="https://lu.ma/s1ah6f4l"
@@ -33,18 +49,19 @@
 </template>
 
 <style>
-
-
 .anime h5 {
-    color: #FAF1E6;
+  color: #faf1e6;
+  font-size: 1.7em;
 }
 
 .anime {
-    transition: .3s transform;
+  transition: 0.3s transform;
+  width: 302px;
+  margin-top: 11px;
 }
 
 .anime:hover {
-    transform: scale(1.05);
+  transform: scale(1.05);
 }
 .rule {
   width: 100%;
@@ -54,16 +71,25 @@
   align-items: center;
 }
 
-.rule-highlight {
-  font-size: 2em;
-}
-
 .rule p {
   width: 80%;
   color: #fff;
   font-size: 1.5em;
 }
+.rule p a{
+  color: #fff;
+  text-decoration: none;
+}
+.rule p a:hover{
+  text-decoration: underline;
+  color: var(--theme1);
+}
 
+.highlight {
+  font-size: 2em !important;
+  color: var(--theme1) !important;
+  font-weight: 600 !important;
+}
 p span {
   color: var(--theme1);
   font-weight: 500;
@@ -75,9 +101,18 @@ p span {
   font-size: 1.4em;
 }
 
-.speaker{
+.speaker {
   width: 100%;
   display: flex;
+}
+
+.card-text .btn {
+  background-color: var(--theme1);
+  width: 90%;
+  color: #000;
+}
+.card-text .btn:hover {
+  background-color: #fff;
 }
 </style>
 
@@ -100,6 +135,8 @@ export default {
             github: "alwenpy",
           },
         ],
+        about: "Postman is an API platform for building and using APIs. Postman simplifies each step of the API lifecycle and streamlines collaboration so you can create better APIs—faster.",
+        link: "https://www.postman.com/"
       },
     };
   },
